30de8ad2e781c8bfcfd48ba1fd34b56140329a0d
Javier Marte <kaison54@hotmail.com>
2016-09-06 23:42:39 -0400

    Made some canges on the main layout

--- app/src/main/java/com/example/kaiso/arduinosimpleled/MainActivity.java
+++ app/src/main/java/com/example/kaiso/arduinosimpleled/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.kaiso.arduinosimpleled;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String json_urld,s2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        s2 = "192.168.240.1";
+        json_urld = "http://"+ s2 +"/arduino/digital/13/0";
+
+    }
+
+    public void ledOne(View view){
+
+
+    }
+    public void ledTwo(View view){
+
+
+    }
+    public void ledThree(View view){
+
+
+    }
+    public void ledFour(View view){
+
+
+    }
+    public void ledFive(View view){
+
+
+    }
+    public void ledSix(View view){
+
+
     }
 }
